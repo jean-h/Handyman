2dbd6aa807b03b56c2d3862de8cef778eb4c0b20
jean-h <jeanniehickman@msn.com>
2021-06-26 00:09:46 -0600

    Update About.js
    Update to use new Logo

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,16 @@ export default function About(){
           <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                Hi, I'm Tri-Lakes Handyman.
-                <br className="hidden lg:inline-block" />I love to help you get things off your list.
+                Tri-Lakes Handyman
+                <br className="hidden lg:inline-block" /> Get things off your list and onto ours.
               </h1>
               <p className="mb-8 leading-relaxed">
-                Yard Work, Painting, Fences, Woodworking, small home projects, whatever you need, give me a call.
+                Yard Work, Painting, Fences, Woodworking, Small Home Projects, whatever you need, give us a call.
               </p>
               <div className="flex justify-center">
                 <a
                   href="#contact"
-                  className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                  className="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
                   Work With Me
                 </a>
                 <a
@@ -25,11 +25,11 @@ export default function About(){
                 </a>
               </div>
             </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 bg-white">
               <img
                 className="object-cover object-center rounded"
                 alt="hero"
-                src="./coding.svg"
+                src="./TriLakes Handyman.png"
               />
             </div>
           </div>
